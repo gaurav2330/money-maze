0301ded774a7afd6430324f3a02b6e58d2d6f132
Gaurav Sharma <sgrb26@gmail.com>
2025-05-27 21:21:24 +0530

    primary key data type change

--- migrations/20250522161855-create-budget.js
+++ migrations/20250522161855-create-budget.js
@@ -5,9 +5,10 @@ module.exports = {
     await queryInterface.createTable('budgets', {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4, // Automatically generates UUIDs
+        primaryKey: true
       },
       name: {
         type: Sequelize.STRING
