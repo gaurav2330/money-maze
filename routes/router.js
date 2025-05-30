f46b7206f95f6a5ad7074ad694115d4ce307c8bc
Gaurav Sharma <sgrb26@gmail.com>
2025-05-30 23:08:29 +0530

    add transaction route

--- routes/router.js
+++ routes/router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { signup, login, logout, validateToken } = require('../controllers/authController')
 const { getAllUsers } = require('../controllers/userController');
 const { createBudget, getBudget } = require('../controllers/budgetController');
+const { createTransaction } = require('../controllers/transactionController');
 
 // Auth routes
 router.post('/signup', signup);
@@ -16,5 +17,8 @@ router.get('/users', getAllUsers);
 // Budget routes
 router.post('/budgets', createBudget);
 router.get('/budgets/:id', getBudget);
+
+// Transaction routes
+router.post('/transactionn', createTransaction);
 
 module.exports = router;
