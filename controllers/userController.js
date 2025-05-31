760a77bdffeeddcc219bbcdd2721b04e74fa37ca
Gaurav Sharma <sgrb26@gmail.com>
2025-05-31 20:00:03 +0530

    feat: Refactor user retrieval to use database instead of JSON file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
+const { sequelize, models } = require('../db');
+const User = models.User;
 
-exports.getAllUsers = (req, res) => {
-  fs.readFile('data/users.json', 'utf-8', (err, data) => {
-    console.log('Read users.json'); 
-    let users = JSON.parse(data);
-    res.status(200).json({users})
-  })
+exports.getAllUsers = async (req, res) => {
+  // Get all users from the database
+  try {
+    const users = await User.findAll();
+    res.status(200).json({ status: 'success', data: users });
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return res.status(500).json({ status: 'error', message: 'Internal server error' });
+  }
 }
